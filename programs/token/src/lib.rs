975a778049279b3073b56ca5763509c79f5a6fe4
dmitry <strukovsky1@gmail.com>
2023-01-23 18:00:00 +0400

    feat: add transfer functions

--- programs/token/src/lib.rs
+++ programs/token/src/lib.rs
@@ -8,21 +8,44 @@ pub mod token {
 
     pub fn initialize(ctx: Context<Initialize>, initial_supply: u64) -> Result<()> {
         let token = &mut ctx.accounts.token;
-        let admin_balance = &mut ctx.accounts.admin_balance;
+        let admin_ownership = &mut ctx.accounts.admin_ownership;
         token.owner = ctx.accounts.signer.key();
         token.total_supply = initial_supply;
-        admin_balance.holder = ctx.accounts.signer.key();
-        admin_balance.balance = initial_supply;
+        admin_ownership.holder = ctx.accounts.signer.key();
+        admin_ownership.balance = initial_supply;
+        Ok(())
+    }
+
+    pub fn transfer_to_empty(ctx: Context<TransferToEmpty>, to: Pubkey, amount: u64) -> Result<()> {
+        let from_account = &mut ctx.accounts.from;
+        require!(from_account.holder == ctx.accounts.signer.key(), TokenErrors::SenderHasNoAccess);
+        let to_account = &mut ctx.accounts.to;
+        require!(to_account.holder == Pubkey::default(), TokenErrors::RecipientAccountNotEmpty);
+        require!(from_account.balance >= amount, TokenErrors::InsufficientBalance);
+        from_account.balance -= amount;
+        to_account.balance = amount;
+        to_account.holder = to;
+        Ok(())
+    }
+
+    pub fn transfer_to_existing(ctx: Context<TransferToExisting>, to: Pubkey, amount: u64) -> Result<()> {
+        let from_account = &mut ctx.accounts.from;
+        require!(from_account.holder == ctx.accounts.signer.key(), TokenErrors::SenderHasNoAccess);
+        let to_account = &mut ctx.accounts.to;
+        require!(to_account.holder == to, TokenErrors::RecipientAccountNotEmpty);
+        require!(from_account.balance >= amount, TokenErrors::InsufficientBalance);
+        from_account.balance -= amount;
+        to_account.balance += amount;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
 pub struct Initialize<'info> {
-    #[account(init, payer=signer, space=256)]
+    #[account(init, payer = signer, space = 256)]
     pub token: Account<'info, Token>,
-    #[account(init, payer=signer, space=256)]
-    pub admin_balance: Account<'info, Balance>,
+    #[account(init, payer = signer, space = 256)]
+    pub admin_ownership: Account<'info, TokenOwnership>,
     #[account(mut)]
     pub signer: Signer<'info>,
     pub system_program: Program<'info, System>,
@@ -35,7 +58,36 @@ pub struct Token {
 }
 
 #[account]
-pub struct Balance {
+pub struct TokenOwnership {
     pub holder: Pubkey,
     pub balance: u64,
+}
+
+#[derive(Accounts)]
+pub struct TransferToEmpty<'info> {
+    #[account(mut)]
+    pub from: Account<'info, TokenOwnership>,
+    #[account(init, payer = signer, space = 256)]
+    pub to: Account<'info, TokenOwnership>,
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct TransferToExisting<'info> {
+    #[account(mut)]
+    pub from: Account<'info, TokenOwnership>,
+    #[account(mut)]
+    pub to: Account<'info, TokenOwnership>,
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[error_code]
+pub enum TokenErrors {
+    SenderHasNoAccess,
+    RecipientAccountNotEmpty,
+    InsufficientBalance,
 }
--- tests/token.ts
+++ tests/token.ts
@@ -1,23 +1,55 @@
 import * as anchor from "@project-serum/anchor";
-import { Program } from "@project-serum/anchor";
-import { Token } from "../target/types/token";
+import {Program} from "@project-serum/anchor";
+import {Token} from "../target/types/token";
+import {expect} from "chai";
 
 describe("token", () => {
-  anchor.setProvider(anchor.AnchorProvider.env());
+    anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace.Token as Program<Token>;
-  const totalSupply = new anchor.BN(10000000);
-  const token = anchor.web3.Keypair.generate();
-  const adminBalance = anchor.web3.Keypair.generate();
+    const program = anchor.workspace.Token as Program<Token>;
+    const totalSupply = new anchor.BN(10000000);
+    const token = anchor.web3.Keypair.generate();
+    const adminOwnership = anchor.web3.Keypair.generate();
+    const recipientOwnership = anchor.web3.Keypair.generate();
+    const recipientKeypair = anchor.web3.Keypair.generate();
 
-  it("Is initialized!", async () => {
-    const tx = await program.methods.initialize(totalSupply)
-        .accounts({
-          token: token.publicKey,
-          adminBalance: adminBalance.publicKey,
-        })
-        .signers([token, adminBalance])
-        .rpc();
-    console.log("Your transaction signature", tx);
-  });
+    it("should be initialized", async () => {
+        const tx = await program.methods.initialize(totalSupply)
+            .accounts({
+                token: token.publicKey,
+                adminOwnership: adminOwnership.publicKey,
+            })
+            .signers([token, adminOwnership])
+            .rpc();
+        console.log("Your transaction signature", tx);
+    });
+
+
+    it("should perform a transfer with non-initialized account", async () => {
+        const transferAmount = new anchor.BN(10);
+
+        await program.methods.transferToEmpty(recipientKeypair.publicKey, transferAmount)
+            .accounts({
+                from: adminOwnership.publicKey,
+                to: recipientOwnership.publicKey,
+            })
+            .signers([recipientOwnership])
+            .rpc();
+
+        let balance = (await program.account.tokenOwnership.fetch(recipientOwnership.publicKey)).balance;
+        expect(transferAmount.eq(balance)).to.be.true;
+    });
+
+    it("should perform a transfer with initialized account", async () => {
+        const transferAmount = new anchor.BN(100);
+        let recipientBalanceBefore = (await program.account.tokenOwnership.fetch(recipientOwnership.publicKey)).balance;
+        await program.methods.transferToExisting(recipientKeypair.publicKey, transferAmount)
+            .accounts({
+                from: adminOwnership.publicKey,
+                to: recipientOwnership.publicKey,
+            })
+            .rpc();
+        let recipientBalanceAfter = (await program.account.tokenOwnership.fetch(recipientOwnership.publicKey)).balance;
+        expect(recipientBalanceAfter.sub(recipientBalanceBefore).eq(transferAmount)).to.be.true;
+    });
 });
